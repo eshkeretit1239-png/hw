ada8659ae16ec0e76eee5d0a3c02764977ee033e
Denis Krylov <eshkeretit1239@gmail.com>
2026-01-22 13:21:56 +0300

    hw 2

--- main.go
+++ main.go
@@ -8,3 +8,12 @@ const convertUsdtToRub float64 = 75.63
 convertUsdrInRub := convertUsdtToRub/convertUsdtToEur
 fmt.Printf("%.2f\n" ,convertUsdrInRub)
 }
+func scanInput()(float64, float64){
+var a, b float64
+fmt.Println("Введите ваши числа")
+fmt.Scan(&a, &b)
+return a,b
+}
+func calculate(float64, float64, float64) (float64){
+	
+}
